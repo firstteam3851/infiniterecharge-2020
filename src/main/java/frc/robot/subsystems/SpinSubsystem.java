161c721b3e3660273dbeb280dd2488017c009d95
Aman Azad <aman.s.azad@gmail.com>
2020-01-28 16:14:26 -0800

    implemented color ranges for kit vinyl

--- src/main/java/frc/robot/subsystems/SpinSubsystem.java
+++ src/main/java/frc/robot/subsystems/SpinSubsystem.java
@@ -1,6 +1,8 @@
 
 package frc.robot.subsystems;
 
+import java.util.Arrays;
+
 import com.revrobotics.ColorSensorV3;
 
 import edu.wpi.first.wpilibj.DriverStation;
@@ -13,6 +15,7 @@ import frc.robot.Constants;
 public class SpinSubsystem extends SubsystemBase {
     Relay spinRelay = new Relay(Constants.SPINNER_RELAY);
     ColorSensorV3 colorSensor = new ColorSensorV3(Constants.COLOR_SENSOR_V3);
+    
     Color detectedColor = null;
 
     public void rotateCounterClockwise() {
@@ -21,6 +24,28 @@ public class SpinSubsystem extends SubsystemBase {
 
     public void rotateClockwise() {
         spinRelay.set(Relay.Value.kForward);
+    }
+
+    public String getColorValue(String rawColorValue) {
+        // Get first character of hash
+        String parsedHashValue = rawColorValue.substring(37, 38);
+        SmartDashboard.putString("ParsedHashValue", parsedHashValue);
+    
+        // match with ranges
+        if (Constants.VINYL_RED_RANGE.contains(parsedHashValue)) {
+            return "RED";
+        }
+        if (Constants.VINYL_GREEN_RANGE.contains(parsedHashValue)) {
+            return "GREEN";
+        }
+        if (Constants.VINYL_BLUE_RANGE.contains(parsedHashValue)) {
+            return "BLUE";
+        }
+        if (Constants.VINYL_YELLOW_RANGE.contains(parsedHashValue)) {
+            return "YELLOW";
+        }
+
+        return "";
     }
 
     public void stop() {
@@ -32,12 +57,14 @@ public class SpinSubsystem extends SubsystemBase {
         // This method will be called once per scheduler run
         detectedColor = colorSensor.getColor();
         double infraredValue = colorSensor.getIR();
-
-        DriverStation.reportWarning(detectedColor.toString(), false);
+        
 
         SmartDashboard.putNumber("Red", detectedColor.red);
         SmartDashboard.putNumber("Green", detectedColor.green);
         SmartDashboard.putNumber("Blue", detectedColor.blue);
         SmartDashboard.putNumber("IR", infraredValue);
+        SmartDashboard.putString("color", detectedColor.toString());
+        SmartDashboard.putString("parsed color", getColorValue(detectedColor.toString()));
+        
     }
 }
